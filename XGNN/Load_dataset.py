2da404f544bc216573b7815bd6426f454277a628
AmGracee <81799751+AmGracee@users.noreply.github.com>
2021-12-16 14:45:26 +0800

    update

--- XGNN/Load_dataset.py
+++ XGNN/Load_dataset.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import scipy.sparse as  sp
 
 def encode_onehot(labels):
     classes = set(labels)
@@ -9,3 +9,10 @@ def encode_onehot(labels):
     labels_onehot = np.array(list(map(classes_dict.get,labels)),
                              dtype=np.int32)
     return labels_onehot
+def normalize(mx):
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum,-1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
